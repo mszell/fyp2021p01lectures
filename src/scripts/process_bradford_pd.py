fde386be6fd5db2962588b1b39ca517c9095923b
mfoman <frederikblunck@gmail.com>
2021-02-12 13:25:34 +0100

    Refactor.

--- src/scripts/process_bradford_pd.py
+++ src/scripts/process_bradford_pd.py
@@ -4,31 +4,38 @@ import pandas as pd
 
 
 # Constants
-raw_folder = Path('./data/raw/')
-pross_folder = Path('./data/processed/')
-pross_out = "bradford_all_inner.csv"
+PATH = {
+    'raw': Path('./data/raw/'),
+    'processed': Path('./data/processed/'),
+}
 
-accident = raw_folder / "Road Safety Data - Accidents 2019.csv"
-casual = raw_folder / "Road Safety Data - Casualties 2019.csv"
-vehicles = raw_folder / "Road Safety Data - Vehicles 2019.csv"
-
+PATH['bradford_out'] = PATH['processed'] / 'bradford_all_inner.csv'
+PATH['accident'] = PATH['raw'] / "Road Safety Data - Accidents 2019.csv"
+PATH['casual'] = PATH['raw'] / "Road Safety Data - Casualties 2019.csv"
+PATH['vehicles'] = PATH['raw'] / "Road Safety Data - Vehicles 2019.csv"
 
 # Process the data
-data_acc = pd.read_csv(
-    accident, dtype={0: 'string', 31: 'string'}, encoding='utf-8-sig')
-data_cas = pd.read_csv(casual, dtype={0: 'string'}, encoding='utf-8-sig')
-data_veh = pd.read_csv(vehicles, dtype={0: 'string'}, encoding='utf-8-sig')
+DATA = {}
+
+DATA['accident'] = pd.read_csv(PATH['accident'], dtype={
+                               0: 'string', 31: 'string'}, encoding='utf-8-sig')
+
+DATA['casual'] = pd.read_csv(PATH['casual'], dtype={
+                             0: 'string'}, encoding='utf-8-sig')
+
+DATA['vehicles'] = pd.read_csv(PATH['vehicles'], dtype={
+                               0: 'string'}, encoding='utf-8-sig')
 
 
 # Merge data with inner join
-brad_acc = data_acc[data_acc["Local_Authority_(District)"] == 200]
+DATA['acc_bradford'] = DATA['accident'][DATA["accident"]
+                                        ["Local_Authority_(District)"] == 200]
 
-data_inner = pd.merge(brad_acc, data_cas, how="inner", on=[
-                      "Accident_Index", "Accident_Index"])
+tmp_inner = pd.merge(DATA['acc_bradford'], DATA['casual'], how="inner", on=[
+    "Accident_Index", "Accident_Index"])
 
-data_inner = pd.merge(data_inner, data_veh, how="inner", on=[
-                      "Accident_Index", "Accident_Index"])
-
+tmp_inner = pd.merge(tmp_inner, DATA['vehicles'], how="inner", on=[
+    "Accident_Index", "Accident_Index"])
 
 # Save all the processed data
-data_inner.to_csv(pross_folder / pross_out)
+tmp_inner.to_csv(PATH['bradford_out'])
